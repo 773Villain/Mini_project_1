65be3e8a4a1a010adfe9934998539542588e4f4a
yourusername <1danieladavila@gmail.com>
2022-09-06 08:48:05 -0500

    This is my finished project

---
+++ sketch.js
@@ -0,0 +1,53 @@
+//This a javascript comment!!!!!!!!!!!!!
+
+
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+  }
+  
+
+console.log("this is my first console log!");
+
+
+
+
+  function draw() {
+background(255,0,255);
+
+circle(50, 25, 200)  
+circle(250, 250, 150)
+circle(350, 400, 100)  
+circle(750, 475, 75)
+circle(425, 500, 50)
+circle(100, 900, 250)
+circle(800, 800, 100)
+circle(600, 650, 250)
+circle(700, 100, 125)
+circle(150, 600, 175)
+
+line(0, 1000, 1000, 0)
+line(0, 1000, 975, 0)
+line(0, 1000, 950, 0)
+line(0, 1000, 925, 0)
+line(0, 1000, 900, 0)
+line(0, 1000, 875, 0)     
+line(0, 1000, 850, 0)
+line(0, 1000, 825, 0)
+line(0, 1000, 800, 0)
+line(0, 1000, 775, 0)
+line(0, 1000, 750, 0)
+line(0, 1000, 725, 0)
+line(0, 1000, 700, 0)
+line(0, 1000, 675, 0)
+line(0, 1000, 650, 0)
+line(0, 1000, 625, 0)
+line(0, 1000, 600, 0)
+line(0, 1000, 575, 0)
+line(0, 1000, 550, 0)
+}
+
+
+
+
+
